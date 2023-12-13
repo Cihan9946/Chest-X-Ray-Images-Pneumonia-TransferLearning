453e94658a92b394ba0f508056ed8667e9748085
Cihan9946 <113993331+Cihan9946@users.noreply.github.com>
2023-12-13 12:04:31 +0300

    Add files via upload

---
+++ Chest-X-Ray-Images-Pneumonia-TransferLearning/Chest-X-Ray-Images-Pneumonia-TransferLearning.py
@@ -0,0 +1,437 @@
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+%matplotlib inline
+import seaborn as sns
+sns.set_style("whitegrid")
+import warnings
+warnings.filterwarnings("ignore")
+import tensorflow as tf
+import glob
+from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
+
+train_dir = "../input/chest-xray-pneumonia/chest_xray/train"
+test_dir = "../input/chest-xray-pneumonia/chest_xray/test"
+val_dir = "../input/chest-xray-pneumonia/chest_xray/val"
+print("Dataset loaded!!")
+
+print("Train set:")
+print("-"*60)
+num_pneumonia = len(os.listdir(os.path.join(train_dir, 'PNEUMONIA')))
+num_normal = len(os.listdir(os.path.join(train_dir, 'NORMAL')))
+print(f"PNEUMONIA={num_pneumonia}")
+print(f"NORMAL={num_normal}")
+
+print("\nTest set:")
+print('-'*60)
+print(f"PNEUMONIA={len(os.listdir(os.path.join(test_dir, 'PNEUMONIA')))}")
+print(f"NORMAL={len(os.listdir(os.path.join(test_dir, 'NORMAL')))}")
+
+print("\nValidation set")
+print('-'*60)
+print(f"PNEUMONIA={len(os.listdir(os.path.join(val_dir, 'PNEUMONIA')))}")
+print(f"NORMAL={len(os.listdir(os.path.join(val_dir, 'NORMAL')))}")
+
+pneumonia = os.listdir("../input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA")
+pneumonia_dir = "../input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA"
+
+plt.figure(figsize=(15, 5))
+
+for i in range(9):
+    plt.subplot(3, 3, i + 1)
+    img = plt.imread(os.path.join(pneumonia_dir, pneumonia[i]))
+    plt.title("PNEUMONIA")
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
+    
+
+plt.tight_layout()
+
+
+normal = os.listdir("../input/chest-xray-pneumonia/chest_xray/train/NORMAL")
+normal_dir = "../input/chest-xray-pneumonia/chest_xray/train/NORMAL"
+
+plt.figure(figsize=(10, 5))
+
+for i in range(9):
+    plt.subplot(3, 3, i + 1)
+    img = plt.imread(os.path.join(pneumonia_dir, pneumonia[i]))
+    plt.title("NORMAL")
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
+    
+plt.tight_layout()
+
+import glob
+
+pneumonia_train = glob.glob(train_dir+"/PNEUMONIA/*.jpeg")
+normal_train = glob.glob(train_dir+"/NORMAL/*.jpeg")
+
+data = pd.DataFrame(np.concatenate([[0]*len(normal_train) , [1]*len(pneumonia_train)]),columns=["class"])
+
+data.head()
+
+img_Datagen = ImageDataGenerator(
+        rescale = 1/255,
+        shear_range=10,
+        zoom_range=0.3,
+        horizontal_flip=True,
+        vertical_flip=True,
+        brightness_range=[0.5,2.0],
+        width_shift_range = 0.2,
+        rotation_range=20,
+        fill_mode = 'nearest'
+)
+val_Datagen = ImageDataGenerator(
+        rescale = 1/255
+)
+train = img_Datagen.flow_from_directory(train_dir,
+                                       batch_size=32,
+                                       class_mode='binary',
+#                                        target_size=(224,224,3))
+                                       )
+
+validation = val_Datagen.flow_from_directory(val_dir,
+                                              batch_size=2,
+                                              class_mode='binary',
+#                                               target_size=(224,224,3))
+                                            )
+
+test = val_Datagen.flow_from_directory(test_dir,
+                                       batch_size=2,
+                                       class_mode='binary',
+#                                        target_size=(224/,224,3))
+                                      )
+
+img, label = next(train)
+
+vgg_model = tf.keras.applications.VGG19(
+    weights='imagenet',
+    include_top = False,
+#     input_shape = (224,224,3)
+)
+
+for layer in vgg_model.layers:
+    layer.trainable=False
+    
+x = vgg_model.output
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dense(128,activation='relu')(x)
+# output layer
+predictions = tf.keras.layers.Dense(1,activation='sigmoid')(x)
+
+model = tf.keras.Model(inputs=vgg_model.input, outputs=predictions)
+
+# to avoid overfitting
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=10)
+lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss',patience=8)
+
+# Compiling the model
+model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+
+model.summary()
+
+history = model.fit(train,epochs=30, 
+                    validation_data=validation,
+                     steps_per_epoch=100,
+                    callbacks=[early_stopping,lr],
+                    batch_size=32)
+
+# Evaluating the model on train and test
+score = model.evaluate(train)
+
+print("Train Loss: ", score[0])
+print("Train Accuracy: ", score[1])
+
+# Test data
+score = model.evaluate(test)
+
+print("Test Loss: ", score[0])
+print("Test Accuracy: ", score[1])
+
+plt.figure(figsize=(12, 8))
+plt.title('EVALUATION OF VGG19')
+
+plt.subplot(2, 2, 1)
+plt.plot(history.history['loss'], label='Loss')
+plt.plot(history.history['val_loss'], label='Val_Loss')
+plt.legend()
+plt.title('Loss Evolution')
+
+plt.subplot(2, 2, 2)
+plt.plot(history.history['accuracy'], label='Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val_Accuracy')
+plt.legend()
+plt.title('Accuracy Evolution')
+
+resnet_model = tf.keras.applications.ResNet50V2(
+    weights='imagenet',
+    include_top = False,
+    input_shape = (224,224,3)
+)
+
+for layer in resnet_model.layers:
+    layer.trainable=False
+    
+x = resnet_model.output
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dense(128,activation='relu')(x)
+# output layer
+predictions = tf.keras.layers.Dense(1,activation='sigmoid')(x)
+
+model2 = tf.keras.Model(inputs=resnet_model.input, outputs=predictions)
+
+# to avoid overfitting
+# early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=6)
+
+# Compiling the model
+model2.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+
+history = model2.fit(train,epochs=30, 
+                    validation_data=validation,
+                     steps_per_epoch=100,
+                    callbacks=[early_stopping,lr],
+                    batch_size=32)
+
+# Evaluating the model on train and test
+score = model2.evaluate(train)
+
+print("Train Loss: ", score[0])
+print("Train Accuracy: ", score[1])
+
+score = model2.evaluate(test)
+print("\nTest loss: ", score[0])
+print("Test Accuracy: ", score[1])
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.plot(history.history['loss'], label='Loss')
+plt.plot(history.history['val_loss'], label='Val_Loss')
+plt.legend()
+plt.title('Loss Evolution')
+
+plt.subplot(2, 2, 2)
+plt.plot(history.history['accuracy'], label='Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val_Accuracy')
+plt.legend()
+plt.title('Accuracy Evolution')
+
+mobilenet_model = tf.keras.applications.MobileNetV2(
+    weights='imagenet',
+    include_top = False,
+#     input_shape = (224,224,3)
+)
+
+for layer in mobilenet_model.layers:
+    layer.trainable=False
+    
+x = mobilenet_model.output
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dense(128,activation='relu')(x)
+# output layer
+predictions = tf.keras.layers.Dense(1,activation='sigmoid')(x)
+
+model3 = tf.keras.Model(inputs=mobilenet_model.input, outputs=predictions)
+
+# to avoid overfitting
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=10)
+lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss',patience=8)
+
+# Compiling the model
+model3.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+history = model3.fit(train,epochs=30, 
+                    validation_data=validation,
+                     steps_per_epoch=100,
+                    callbacks=[early_stopping,lr],
+                    batch_size=32)
+
+# Evaluating the model on train and test
+score = model3.evaluate(train)
+
+print("Train Loss: ", score[0])
+print("Train Accuracy: ", score[1])
+
+score = model3.evaluate(test)
+print("\nTest loss: ", score[0])
+print("Test Accuracy: ", score[1])
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.plot(history.history['loss'], label='Loss')
+plt.plot(history.history['val_loss'], label='Val_Loss')
+plt.legend()
+plt.title('Loss Evolution')
+
+plt.subplot(2, 2, 2)
+plt.plot(history.history['accuracy'], label='Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val_Accuracy')
+plt.legend()
+plt.title('Accuracy Evolution')
+
+# Saving these 3 models
+model.save('vgg19.h5')
+model2.save('resnet50v2.h5')
+model3.save('mobilenetv2.h5')
+
+resnet_model.trainable = True
+vgg_model.trainable = True
+mobilenet_model.trainable = True
+
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(vgg_model.layers))
+
+# Fine-tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in vgg_model.layers[:fine_tune_at]:
+  layer.trainable = False
+
+
+model.summary()
+
+# Compiling the model
+model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+len(model.trainable_variables)
+
+fine_tune_epochs = 10
+epochs_on_raw_model = 30
+
+total_epochs =  epochs_on_raw_model + fine_tune_epochs
+
+history_fine =model.fit(train,epochs=total_epochs, 
+                    validation_data=validation,
+                    steps_per_epoch=100,
+                    callbacks=[early_stopping,lr],
+                    batch_size=32)
+
+# Evaluating the model on train and test
+score = model.evaluate(train)
+
+print("Train Loss: ", score[0])
+print("Train Accuracy: ", score[1])
+
+score = model.evaluate(test)
+print("\nTest loss: ", score[0])
+print("Test Accuracy: ", score[1])
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.plot(history_fine.history['loss'], label='Loss')
+plt.plot(history_fine.history['val_loss'], label='Val_Loss')
+plt.legend()
+plt.title('Loss Evolution')
+
+plt.subplot(2, 2, 2)
+plt.plot(history_fine.history['accuracy'], label='Accuracy')
+plt.plot(history_fine.history['val_accuracy'], label='Val_Accuracy')
+plt.legend()
+plt.title('Accuracy Evolution')
+
+# Fine tuning the resnet model
+
+
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(resnet_model.layers))
+
+# Fine-tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in resnet_model.layers[:fine_tune_at]:
+  layer.trainable = False
+
+model2.summary()
+
+# Compiling the model
+model2.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+
+len(model2.trainable_variables)
+
+fine_tune_epochs = 10
+epochs_on_raw_model = 30
+
+total_epochs =  epochs_on_raw_model + fine_tune_epochs
+
+history_fine =model2.fit(train,epochs=total_epochs, 
+                    validation_data=validation,
+                    steps_per_epoch=100,
+                    callbacks=[early_stopping,lr],
+                    batch_size=32)
+
+# Evaluating the model on train and test
+score = model2.evaluate(train)
+
+print("Train Loss: ", score[0])
+print("Train Accuracy: ", score[1])
+
+score = model2.evaluate(test)
+print("\nTest loss: ", score[0])
+print("Test Accuracy: ", score[1])
+
+# Fine tuning the mobilenetv2
+
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(mobilenet_model.layers))
+
+# Fine-tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in mobilenet_model.layers[:fine_tune_at]:
+  layer.trainable = False
+
+# Compiling the model
+model3.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+len(model3.trainable_variables)
+
+fine_tune_epochs = 10
+epochs_on_raw_model = 30
+
+total_epochs =  epochs_on_raw_model + fine_tune_epochs
+
+history_fine =model3.fit(train,epochs=total_epochs, 
+                    validation_data=validation,
+                    steps_per_epoch=100,
+                    callbacks=[early_stopping,lr],
+                    batch_size=32)
+
+# Evaluating the model on train and test
+score = model3.evaluate(train)
+
+print("Train Loss: ", score[0])
+print("Train Accuracy: ", score[1])
+
+score = model3.evaluate(test)
+print("\nTest loss: ", score[0])
+print("Test Accuracy: ", score[1])
+
+
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 2, 1)
+plt.plot(history_fine.history['loss'], label='Loss')
+plt.plot(history_fine.history['val_loss'], label='Val_Loss')
+plt.legend()
+plt.title('Loss Evolution')
+
+plt.subplot(2, 2, 2)
+plt.plot(history_fine.history['accuracy'], label='Accuracy')
+plt.plot(history_fine.history['val_accuracy'], label='Val_Accuracy')
+plt.legend()
+plt.title('Accuracy Evolution')
+
+# Saving the fine tuned model
+model.save('tuned_vgg19.h5')
+model2.save('tuned_resnet.h5')
+model3.save('tuned_mobilenet.h5')
+
+# Loading the saved version of Tuned-ResNet50V2
+final_model = tf.keras.models.load_model('tuned_resnet.h5')
+y_pred = final_model.predict(test)
+final_model.evaluate(test)
