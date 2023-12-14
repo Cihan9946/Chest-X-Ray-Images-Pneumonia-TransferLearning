7c7a1964b32735ad1d16d974e8432612f4634042
Cihan9946 <113993331+Cihan9946@users.noreply.github.com>
2023-12-14 13:08:11 +0300

    Update Chest-X-Ray-Images-Pneumonia-TransferLearning.py

--- Chest-X-Ray-Images-Pneumonia-TransferLearning/Chest-X-Ray-Images-Pneumonia-TransferLearning.py
+++ Chest-X-Ray-Images-Pneumonia-TransferLearning/Chest-X-Ray-Images-Pneumonia-TransferLearning.py
@@ -12,10 +12,12 @@ import tensorflow as tf
 import glob
 from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
 
+
 train_dir = "../input/chest-xray-pneumonia/chest_xray/train"
 test_dir = "../input/chest-xray-pneumonia/chest_xray/test"
 val_dir = "../input/chest-xray-pneumonia/chest_xray/val"
 print("Dataset loaded!!")
+
 
 print("Train set:")
 print("-"*60)
@@ -24,20 +26,25 @@ num_normal = len(os.listdir(os.path.join(train_dir, 'NORMAL')))
 print(f"PNEUMONIA={num_pneumonia}")
 print(f"NORMAL={num_normal}")
 
+
 print("\nTest set:")
 print('-'*60)
 print(f"PNEUMONIA={len(os.listdir(os.path.join(test_dir, 'PNEUMONIA')))}")
 print(f"NORMAL={len(os.listdir(os.path.join(test_dir, 'NORMAL')))}")
+
 
 print("\nValidation set")
 print('-'*60)
 print(f"PNEUMONIA={len(os.listdir(os.path.join(val_dir, 'PNEUMONIA')))}")
 print(f"NORMAL={len(os.listdir(os.path.join(val_dir, 'NORMAL')))}")
 
+
 pneumonia = os.listdir("../input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA")
 pneumonia_dir = "../input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA"
 
+
 plt.figure(figsize=(15, 5))
+
 
 for i in range(9):
     plt.subplot(3, 3, i + 1)
@@ -47,13 +54,17 @@ for i in range(9):
     plt.axis('off')
     
 
+
 plt.tight_layout()
+
 
 
 normal = os.listdir("../input/chest-xray-pneumonia/chest_xray/train/NORMAL")
 normal_dir = "../input/chest-xray-pneumonia/chest_xray/train/NORMAL"
 
+
 plt.figure(figsize=(10, 5))
+
 
 for i in range(9):
     plt.subplot(3, 3, i + 1)
@@ -61,17 +72,23 @@ for i in range(9):
     plt.title("NORMAL")
     plt.imshow(img, cmap='gray')
     plt.axis('off')
-    
+
+
 plt.tight_layout()
 
+
 import glob
+
 
 pneumonia_train = glob.glob(train_dir+"/PNEUMONIA/*.jpeg")
 normal_train = glob.glob(train_dir+"/NORMAL/*.jpeg")
 
+
 data = pd.DataFrame(np.concatenate([[0]*len(normal_train) , [1]*len(pneumonia_train)]),columns=["class"])
 
+
 data.head()
+
 
 img_Datagen = ImageDataGenerator(
         rescale = 1/255,
@@ -84,14 +101,17 @@ img_Datagen = ImageDataGenerator(
         rotation_range=20,
         fill_mode = 'nearest'
 )
+
 val_Datagen = ImageDataGenerator(
         rescale = 1/255
 )
+
 train = img_Datagen.flow_from_directory(train_dir,
                                        batch_size=32,
                                        class_mode='binary',
 #                                        target_size=(224,224,3))
                                        )
+
 
 validation = val_Datagen.flow_from_directory(val_dir,
                                               batch_size=2,
@@ -99,13 +119,16 @@ validation = val_Datagen.flow_from_directory(val_dir,
 #                                               target_size=(224,224,3))
                                             )
 
+
 test = val_Datagen.flow_from_directory(test_dir,
                                        batch_size=2,
                                        class_mode='binary',
 #                                        target_size=(224/,224,3))
                                       )
 
+
 img, label = next(train)
+
 
 vgg_model = tf.keras.applications.VGG19(
     weights='imagenet',
@@ -113,16 +136,20 @@ vgg_model = tf.keras.applications.VGG19(
 #     input_shape = (224,224,3)
 )
 
+
 for layer in vgg_model.layers:
     layer.trainable=False
-    
+
+
 x = vgg_model.output
 x = tf.keras.layers.GlobalAveragePooling2D()(x)
 x = tf.keras.layers.Dense(128,activation='relu')(x)
+
 # output layer
 predictions = tf.keras.layers.Dense(1,activation='sigmoid')(x)
 
 model = tf.keras.Model(inputs=vgg_model.input, outputs=predictions)
+
 
 # to avoid overfitting
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=10)
@@ -133,17 +160,20 @@ model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
 
 model.summary()
 
+
 history = model.fit(train,epochs=30, 
                     validation_data=validation,
                      steps_per_epoch=100,
                     callbacks=[early_stopping,lr],
                     batch_size=32)
 
+
 # Evaluating the model on train and test
 score = model.evaluate(train)
 
 print("Train Loss: ", score[0])
 print("Train Accuracy: ", score[1])
+
 
 # Test data
 score = model.evaluate(test)
@@ -160,11 +190,13 @@ plt.plot(history.history['val_loss'], label='Val_Loss')
 plt.legend()
 plt.title('Loss Evolution')
 
+
 plt.subplot(2, 2, 2)
 plt.plot(history.history['accuracy'], label='Accuracy')
 plt.plot(history.history['val_accuracy'], label='Val_Accuracy')
 plt.legend()
 plt.title('Accuracy Evolution')
+
 
 resnet_model = tf.keras.applications.ResNet50V2(
     weights='imagenet',
@@ -172,22 +204,28 @@ resnet_model = tf.keras.applications.ResNet50V2(
     input_shape = (224,224,3)
 )
 
+
 for layer in resnet_model.layers:
     layer.trainable=False
     
+
 x = resnet_model.output
 x = tf.keras.layers.GlobalAveragePooling2D()(x)
 x = tf.keras.layers.Dense(128,activation='relu')(x)
 # output layer
 predictions = tf.keras.layers.Dense(1,activation='sigmoid')(x)
 
+
 model2 = tf.keras.Model(inputs=resnet_model.input, outputs=predictions)
 
+
 # to avoid overfitting
+
 # early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=6)
 
 # Compiling the model
 model2.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+
 
 history = model2.fit(train,epochs=30, 
                     validation_data=validation,
@@ -196,22 +234,28 @@ history = model2.fit(train,epochs=30,
                     batch_size=32)
 
 # Evaluating the model on train and test
+
 score = model2.evaluate(train)
+
 
 print("Train Loss: ", score[0])
 print("Train Accuracy: ", score[1])
+
 
 score = model2.evaluate(test)
 print("\nTest loss: ", score[0])
 print("Test Accuracy: ", score[1])
 
+
 plt.figure(figsize=(12, 8))
+
 
 plt.subplot(2, 2, 1)
 plt.plot(history.history['loss'], label='Loss')
 plt.plot(history.history['val_loss'], label='Val_Loss')
 plt.legend()
 plt.title('Loss Evolution')
+
 
 plt.subplot(2, 2, 2)
 plt.plot(history.history['accuracy'], label='Accuracy')
@@ -225,9 +269,11 @@ mobilenet_model = tf.keras.applications.MobileNetV2(
 #     input_shape = (224,224,3)
 )
 
+
 for layer in mobilenet_model.layers:
     layer.trainable=False
     
+
 x = mobilenet_model.output
 x = tf.keras.layers.GlobalAveragePooling2D()(x)
 x = tf.keras.layers.Dense(128,activation='relu')(x)
